25f9011ad0553d4ca136a3d96ffa3d3deaa549f0
Luis Pereira <luispereira21@live.com.pt>
2026-07-22 15:59:55 +0100

    Atualização

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class LegoApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Lego App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
