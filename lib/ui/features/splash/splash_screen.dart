fe44a3903605122896709999d5a947a2c4a743d6
Luis Pereira <luispereira21@live.com.pt>
2026-07-27 16:36:50 +0100

    Atualização

--- lib/ui/features/splash/splash_screen.dart
+++ lib/ui/features/splash/splash_screen.dart
@@ -93,7 +93,7 @@ class _SplashScreenState extends State<SplashScreen> {
               const SizedBox(height: 16),
 
               Text(
-                'LEGO Collector',
+                'LEGO COLLECTOR',
                 style: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
